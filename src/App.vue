48623a94cbacd944f1045be1915935c920abedb7
ncanatta <ncanatta@gmail.com>
2019-10-18 16:17:15 -0300

    Quick start project for sass

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,47 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+  <t-layout row>
+    <t-aside :is-open="isOpen" :width="350" :padding-top="30" is-dark>
+      <t-flex is-centered is-column>
+        <t-image :src="getLogo" size="is-128x128"></t-image>
+      </t-flex>
+    </t-aside>
+    <t-layout class="content">
+      <t-header is-link>
+        <t-navbar is-primary transparent :max-height="52">
+          <t-navbar-items end>
+            <t-navbar-burger
+              :active="isOpen"
+              @click="toggleAside"
+            ></t-navbar-burger>
+          </t-navbar-items>
+        </t-navbar>
+      </t-header>
+      <t-content>
+        <Home></Home>
+      </t-content>
+    </t-layout>
+  </t-layout>
 </template>
-
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import logo from "./assets/logo.svg";
+import Home from "@/components/Home";
 
 export default {
-  name: 'app',
-  components: {
-    HelloWorld
+  components: { Home },
+  data() {
+    return {
+      isOpen: false
+    };
+  },
+  computed: {
+    getLogo() {
+      return logo;
+    }
+  },
+  methods: {
+    toggleAside() {
+      this.isOpen = !this.isOpen;
+    }
   }
-}
+};
 </script>
-
-<style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
---
+++ src/components/Home.vue
@@ -0,0 +1,51 @@
+<template>
+  <t-hero
+    :title="title"
+    :description="description"
+    :img="getLogo"
+    is-fullheight
+    is-dark
+    linear
+  >
+    <template slot="action">
+      <t-button
+        href="http://www.thisvui.com"
+        target="_blank"
+        rel="noopener"
+        icon="fas fa-file"
+        is-success
+        is-inline-flex
+      >
+        Docs
+      </t-button>
+      <t-button
+        href="https://github.com/thisvui/thisvui"
+        target="_blank"
+        rel="noopener"
+        icon="fab fa-github"
+        is-dark
+        is-inline-flex
+      >
+        GitHub
+      </t-button>
+    </template>
+  </t-hero>
+</template>
+<script>
+import logo from "../assets/logo.svg";
+
+export default {
+  name: "Home",
+  data() {
+    return {
+      title: `Welcome!`,
+      description: `Welcome to Your ThisVui App. Check the docs for a guide and recipes on how to build amazing UI.`
+    };
+  },
+  computed: {
+    getLogo() {
+      return logo;
+    }
+  }
+};
+</script>
